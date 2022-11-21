5def9ef766f470363ca6c39f4a4b8e8840b5e6c6
tropical9 <113566952+tropical9@users.noreply.github.com>
2022-11-21 00:40:15 -0800

    html updates

--- script.js
+++ script.js
@@ -8,8 +8,11 @@
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // useful when saving the description in local storage?  
   //
+
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
